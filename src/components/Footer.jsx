af01fd648303e18cb6b0ae95d4ff892aad74115d
Diego Tellez <diegotellezcontreras@gmail.com>
2023-07-26 20:58:44 -0500

    Fixed some styles in footer and header

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <footer className='mt-auto flex flex-col items-center text-dark-green mb-6 gap-2 md:flex-row md:justify-between px-40'>
+        <footer className='mt-auto flex flex-col items-center text-dark-green mb-6 gap-2 md:flex-row md:justify-between md:px-40'>
             
             <div className='flex gap-x-4 md:order-last'>
                 <a className='text-4xl text-secondary-color hover:text-primary-color hover:animate-bounce' target='_blank' href="https://github.com/diegotellezc">
@@ -18,7 +18,7 @@ const Footer = () => {
                 </a>
             </div>
 
-            <p className='md:order-2 text-secondary-color'>• Copyright ©2023 | Todos los derechos reservados •</p>
+            <p className='md:order-2 text-secondary-color hidden lg:block'>• Copyright ©2023 | Todos los derechos reservados •</p>
             <p className='text-secondary-color'>@diegotellezc</p>
         </footer>
     )
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,11 +4,11 @@ import { Link } from 'react-router-dom'
 const Header = () => {
     return (
         <header className='bg-secondary-color w-full m-0 p-4 text-center flex justify-center items-center gap-4 md:flex-row md:justify-between'>
-          <div className='w-[15rem] ml-20 py-3'>
+          <div className='w-[240px] md:ml-20 py-3 hidden sm:block'>
             <img src="/logo-fianzas.png" alt="logo-fianzas" className='w-full' />
           </div>
 
-          <nav className='text-white flex gap-6 mr-20'>
+          <nav className='text-white flex gap-6 md:mr-20'>
             <Link to={"/"} className='hover:text-primary-color'>Asesorías</Link>
             <Link to={"/sugerencias"} className='hover:text-primary-color'>Sugerencias</Link>
             <Link to={"/reclamos"} className='hover:text-primary-color'>Reclamos</Link>
